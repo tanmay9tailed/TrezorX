9d0831b09f22fc07237b5286a074c5e1ed697b55
Tanmay Sahu <sahutanmay1903@gmail.com>
2024-08-21 18:08:18 +0530

    corrected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ import { useRouter } from "next/navigation";
 
 export default function Home() {
   const { theme } = useTheme();
-  const [t, setT] = useState("");
-  const router = useRouter()
+  const [t, setT] = useState("dark"); // Default value is 'dark'
+  const router = useRouter();
 
   useEffect(() => {
-    setT(theme);
+    setT(theme ?? "dark");
   }, [theme]);
 
   return (
